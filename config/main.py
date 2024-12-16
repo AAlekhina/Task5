1fd3b5d0fb8623ab2e728858f4fff20f7553610a
AAlekhina <ae.alekhina@gmail.com>
2024-12-16 10:35:08 +0100

    Add files via upload
    file for configuring access to PostgreSQL

---
+++ config/main.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+# Подключение к локальной базе данных PostgreSQL
+conn = psycopg2.connect(
+    dbname = 'task4',
+    user = 'postgres',
+    password = 'postgres',
+    host = 'localhost',
+    port = '5432'  # default is 5432
+)
+
+# Создаем курсор для выполнения SQL-запросов
+cursor = conn.cursor()
+
+# Пример запроса
+cursor.execute("SELECT version();")
+
+# Получение результата запроса
+record = cursor.fetchone()
+print(f"Вы подключены к - {record}")
+
+# Закрываем соединение
+cursor.close()
+conn.close()
